5fe7a8841c53cf690b8fbd95b38860c7b91fa07b
singletonn <sopranowest@gmail.com>
2018-07-16 16:14:48 +1000

    Убрал стили теперь голый bootstrap

--- widgets/geolocation/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.region-link-gray, .region-arrow', function () {
-    var self = $(this);
-    var dropDown = $('#select_region');
-    dropDown.fadeToggle(300);
-    self.toggleClass('active');
-    return false;
-});
--- widgets/geolocation/views/index.php
+++ widgets/geolocation/views/index.php
@@ -13,23 +13,21 @@ use yii\web\View;
 /* @var $this View */
 /* @var $popularCities GeobaseCity[] */
 ?>
-<div id="my-region">
-    <div id="your_city">
-        <div id="my-region-link">
-            <?php if (is_null(Yii::$app->geolocation->get())): ?>
-                <span>
+<div class="geolocation">
+    <div class="dropdown geolocation__dropdown">
+        <?php if (is_null(Yii::$app->geolocation->get())): ?>
+            <div class="geolocation__dropdown-header">
                 <?php if (Yii::$app->geolocation->identify()): ?>
                     Ваш регион <?= Yii::$app->geolocation->identify()->name ?>?
                 <?php else: ?>
                     Мы не смогли определить ваш город
                 <?php endif; ?>
-                </span>
-            <?php else: ?>
-                <a href="javascript:void(0)" class="region-arrow">
-                    г. <?= Yii::$app->geolocation->get()->name ?>
-                </a>
-            <?php endif; ?>
-        </div>
+            </div>
+        <?php else: ?>
+            <a href="javascript:void(0)" data-toggle="dropdown">
+                г. <?= Yii::$app->geolocation->get()->name ?>
+            </a>
+        <?php endif; ?>
         <?php if (is_null(Yii::$app->geolocation->get())): ?>
             <?php
             if (Yii::$app->geolocation->identify()) {
@@ -37,48 +35,45 @@ use yii\web\View;
                     '/geolocation/default/set',
                     'id' => Yii::$app->geolocation->identify()->id,
                 ], [
-                    'class' => 'region-link',
+                    'class' => 'btn btn-success',
                 ]);
             }
             ?>
-            <a href="javascript:void(0)" class="region-link-gray">
-            <span class="region-arrow">
+            <button data-toggle="dropdown" class="btn btn-default">
                 Изменить регион
-            </span>
-            </a>
+                <span class="caret"></span>
+            </button>
         <?php endif; ?>
-    </div>
-    <div id="select_region" style="display: none;">
-        <div class="h1">Выбор города</div>
-        <div class="notice">Всего <?= Yii::$app->geolocation->getCountCity() ?> город</div>
-        <div id="search-city">
+        <div class="dropdown-menu geolocation__dropdown-menu" aria-labelledby="dLabel">
+            <div class="geolocation__dropdown-menu-header">Выбор города</div>
+            <div class="geolocation__dropdown-counter">Всего <?= Yii::$app->geolocation->getCountCity() ?> город</div>
             <?= \yii\jui\AutoComplete::widget([
                 'options' => [
-                    'class' => 'form-control',
+                    'class' => 'geolocation__dropdown-search form-control',
                 ],
                 'clientOptions' => [
                     'source' => \yii\helpers\Url::to(['/geolocation/default/search']),
                     'minLength' => '3',
                     'select' => new \yii\web\JsExpression('
-                        function (event, ui) {
-                            window.location = "/geolocation/default/set/" + ui.item.value;
-                            return false;
-                        }
-                    '),
+                    function (event, ui) {
+                        window.location = "/geolocation/default/set/" + ui.item.value;
+                        return false;
+                    }
+                '),
                     'focus' => new \yii\web\JsExpression('
-                        function(event, ui){
-                            return false;
-                        }
-                    '),
+                    function(event, ui){
+                        return false;
+                    }
+                '),
                 ],
             ]) ?>
+            <ul class="geolocation__dropdown-list">
+                <?php foreach ($popularCities as $city): ?>
+                    <li class="geolocation__dropdown-item">
+                        <?= Html::a($city->name, ['/geolocation/default/set', 'id' => $city->id]) ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
-        <ul>
-            <?php foreach ($popularCities as $city): ?>
-                <li>
-                    <?= Html::a($city->name, ['/geolocation/default/set', 'id' => $city->id]) ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
     </div>
 </div>
